1296a7f9f75cd03701bc77705339ec166c847d6a
Gianfranco Gioia <gianfranco.gioia00@gmail.com>
2023-01-13 16:45:56 -0300

    Primer commit

---
+++ Calculadora.py
@@ -0,0 +1,27 @@
+num1 = int(input("Ingrese un numero: "))
+num2 = int(input("Ingrese otro numero: "))
+
+
+#Suma
+resultado = num1 + num2 
+print(f'El resultado de la suma es:',resultado)
+
+#Resta
+resultado = num1 - num2
+print(f'El resultado de la resta es:',resultado)
+
+#Multiplicadion
+resultado = num1 * num2
+print(f'El resultado de la multiplicaion es:',resultado)
+
+#Division 
+resultado = num1 / num2
+print(f'El resultado de la division de estos dos numeros es:',resultado)
+
+#Resto
+resultado = num1 % num2
+print(f'El resto de la division de estos dos numeros es:',resultado)
+
+#Elevado 
+resultado = num1 ** num2
+print(f'El resultado de elevar estos dos numeros es:',resultado)
