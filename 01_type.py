4ad4cc6e5d6bd5a00bacda611369baf3a453622a
Gianfranco Gioia <gianfranco.gioia00@gmail.com>
2023-01-13 23:20:38 -0300

    commit

---
+++ 01_type.py
@@ -0,0 +1,16 @@
+#Tipos de datos en python 
+
+#tipo str --> string 
+print(type("soy un dato str"))
+#tipo int --> entero
+print(type(5))
+#tipo float --> flotante
+print(type(1.5))
+#tipo bool --> boleano
+print(type(True))
+#tipo complex --> numero complejo
+print(type(3 + 1j))
+
+#***tipo NoneType --> tipo de dato nulo***
+print(type(print("Mi cadena de texto")))
+
---
+++ 02_variables.py
@@ -0,0 +1,60 @@
+# Variable string
+my_string_varible = "My String Variable"
+print(my_string_varible)
+
+
+# Variable int
+my_int_variable = 22
+print(my_int_variable)
+# Pasar de varible int a string
+print(str(my_int_variable))
+
+
+# Variable tipo bool
+my_bool_varible = False or True
+print(my_bool_varible)
+
+
+# concatenacion de varibles en un print
+print(my_string_varible,my_int_variable,my_bool_varible)
+
+
+# Funcion del sistema * cuenta la cantidad de caracteres
+print(len(my_string_varible))
+# Se puede definir o especificar con str el print 
+print(f'Esta es la cantidad de caracteres de:',len(my_string_varible))
+
+
+# Variables en una sola linea
+name, surname, alias, edad = "Gianfranco", "Gioia", "byikos", 22
+print(f'Me llamo:',name, surname,f'Mi alias es:', alias,f'Mi edad es:', edad) #Puede cambiar el orden en que se imprime (¡no se recomienda esta sintaxis!)
+
+
+'''
+Esto es un comentario
+
+# Input como variable 
+first_name = input("What is your name?:")
+age = input('How old are you?:')
+
+print(first_name)
+print(age)
+
+
+# forzar el tipo de dato de una variable
+first_name = input("What is your name?:")
+age = input(str('How old are you?:'))
+
+print(first_name)
+print(age)
+print(type(age))
+'''
+
+
+# Las varibles pueden cambiar su valor a la vez que tu tipo de dato
+first_name = 22
+age = "Gianfranco"
+print(first_name)
+print(age)
+
+
